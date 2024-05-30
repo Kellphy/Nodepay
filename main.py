c195093b239a17f3e9c14fa50544f09103f2121b
Kellphy <virtual_44@yahoo.com>
2024-05-31 00:04:19 +0300

    Init

---
+++ main.py
@@ -0,0 +1,140 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import logging
+
+def setup_logging():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def check_active_element(driver):
+    try:
+        wait_for_element(driver,By.XPATH,"//*[text()='Activated']")
+        driver.find_element(By.XPATH, "//*[text()='Activated']")
+        logging.info("Extension is activated!")
+    except NoSuchElementException:
+        logging.error("Failed to find 'Activated' element. Extension activation failed.")
+
+
+def wait_for_element(driver, by, value, timeout=10):
+    """Waits for an element to be present on the page."""
+    try:
+        element = WebDriverWait(driver, timeout).until(EC.presence_of_element_located((by, value)))
+        return element
+    except TimeoutException as e:
+        logging.error(f"Error waiting for element {value}: {e}")
+
+def run():
+    setup_logging()
+    logging.info('Starting the script...')
+
+    # Read variables from the OS env
+    email = os.getenv('NP_USER')
+    password = os.getenv('NP_PASS')
+    extension_id = os.getenv('EXTENSION_ID')
+    extension_url = os.getenv('EXTENSION_URL')
+
+    # Check if credentials are provided
+    if not email or not password:
+        logging.error('No username or password provided. Please set the NP_USER and NP_PASS environment variables.')
+        return  # Exit the script if credentials are not provided
+
+    chrome_options = Options()
+    chrome_options.add_extension(f'./{extension_id}.crx')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless=new')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0")
+
+    # Initialize the WebDriver
+    driver = webdriver.Chrome(options=chrome_options)
+    # NodePass checks for width less than 1024p
+    driver.set_window_size(1024, driver.get_window_size()['height'])
+
+    try:
+        # Navigate to a webpage
+        logging.info(f'Navigating to {extension_url} website...')
+        driver.get(extension_url)
+
+        wait_for_element(driver,By.ID,"basic_user")
+        logging.info('Entering credentials...')
+        username = driver.find_element(By.ID,"basic_user")
+        username.send_keys(email)
+        passwd = driver.find_element(By.ID,"basic_password")
+        passwd.send_keys(password)
+        
+        logging.info('Clicking the login button...')
+        submit = driver.find_element(By.XPATH, "//button[@type='submit']")
+        submit.click()
+        
+        # Check successful login
+        
+        try:
+            wait_for_element(driver,By.XPATH,"//*[text()='Dashboard']")
+            driver.find_element(By.XPATH, "//*[text()='Dashboard']")
+        except NoSuchElementException:
+            logging.error("Failed to find 'Dashbaord' element.")
+            logging.error(driver.page_source)
+            raise Exception("Login failed!")
+
+        logging.info('Logged in successfully!')
+
+        logging.info('Accessing extension settings page...')
+        driver.get(f'chrome-extension://{extension_id}/index.html')
+        
+        # Refresh until the "Login" button disappears
+        while True:
+            try:
+                # Click the "Login" button
+                wait_for_element(driver,By.XPATH,"//*[text()='Login']")
+                logging.info('Clicking the extension login button...')
+                login = driver.find_element(By.XPATH, "//*[text()='Login']")
+                login.click()
+                
+                # Refresh the page
+                driver.refresh()
+                wait_for_element(driver,By.XPATH,"//*[text()='Login']")
+            except (TimeoutException, NoSuchElementException) as e:
+                # "Login" button not found, break out of the loop
+                break
+        
+        # Check for the "Activated" element
+        check_active_element(driver)
+
+        # Get handles for all windows
+        all_windows = driver.window_handles
+
+        # Get the handle of the active window
+        active_window = driver.current_window_handle
+
+        # Close all windows except the active one
+        for window in all_windows:
+            if window != active_window:
+                driver.switch_to.window(window)
+                driver.close()
+
+        # Switch back to the active window
+        driver.switch_to.window(active_window)
+
+        logging.info('Earning...')
+    except Exception as e:
+        logging.error(f'An error occurred: {e}')
+        driver.quit()
+        time.sleep(60)
+        run()
+
+    while True:
+        try:
+            time.sleep(600)
+            driver.refresh()
+            check_active_element(driver)
+        except KeyboardInterrupt:
+            logging.info('Stopping the script...')
+            driver.quit()
+            break
+
+run()
